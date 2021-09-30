17157fc4c5538259bdc334b007015437fa27c3f7
Davide Corsi <davide.corsi@par-tec.it>
2021-09-30 14:16:22 +0200

    first commit

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+
+public class Main {
+	public static void main(String[] args) {
+		int a = 5;
+		int b = -a; // operatore unario prefisso
+		
+		System.out.println(++a);
+		System.out.println(a);
+		System.out.println(a++);
+		System.out.println(a);
+		
+//		System.out.println(--a);
+//		System.out.println(a);
+//		System.out.println(a--);
+//		System.out.println(a);
+		
+//		double x = 7.3;
+//		System.out.println(++x);
+		
+//		char c1 = 'A';
+//		char c2 = ++c1;
+//		System.out.println(c2);
+		
+//		int n = -'A';
+//		System.out.println(n);
+	}
+}
